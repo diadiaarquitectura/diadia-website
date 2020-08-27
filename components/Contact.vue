4fd2bdbdb7d8fc8cbb691c45ff92bfc7e7c063de
Jerson La Torre <jerson.latorre@gmail.com>
2020-08-26 21:18:01 -0500

    dimensiones corregidas

--- components/Contact.vue
+++ components/Contact.vue
@@ -3,7 +3,6 @@
   .content.row
     .pic.col-md-8(:style='{ backgroundImage: "url(" + contactInfo.imagen + ")" }')
     .column.col-md-4
-      //- .title Contacto
       .text(v-html='contactInfo.mensaje')
       .title Estudio
       .text(v-html='contactInfo.direccion')
@@ -32,15 +31,15 @@ export default {
 <style lang="scss" scoped>
 #contact {
   position: absolute;
-  top: 130px;
+  top: 30px;
   width: 100%;
-  height: calc(100%);
+  height: 100%;
   display: flex;
   text-align: center;
   align-items: center;
-  padding-bottom: 100px;
-  padding-right: 10px;
-  padding-left: 10px;
+  // padding-bottom: 100px;
+  // padding-right: 10px;
+  // padding-left: 10px;
 
   .content {
     max-width: 1200px;
@@ -62,17 +61,15 @@ export default {
 
     .pic {
       width: 100%;
-      height: 550px;
+      // height: 550px;
       background-position: center;
       background-size: cover;
-      margin-bottom: 20px;
     }
 
     .column {
       text-align: left;
-      padding-left: 30px;
+      padding-left: 20px;
       padding-right: 10px;
-      padding-top: 10px;
     }
   }
 }
