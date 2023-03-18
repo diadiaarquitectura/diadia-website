499409a46aa5bbe01b73276791a0e3c242d743f9
Jerson La Torre <jerson.latorre@gmail.com>
2023-03-18 16:28:58 -0500

    fix: error de compresión al generar la página

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,16 @@ export default {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1'
+        content:
+          'width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1',
       },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
 
     script: [
@@ -21,18 +26,14 @@ export default {
       { src: 'https://rawgit.com/metafizzy/isotope-packery/master/packery-mode.pkgd.js' },
       { src: 'https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.3.1/dist/lazyload.min.js' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   loadingIndicator: false,
-  css: [],
-  plugins: [],
-  buildModules: [],
-  modules: [ 'bootstrap-vue/nuxt', '@nuxtjs/axios' ],
-  axios: {},
+  modules: ['bootstrap-vue/nuxt', '@nuxtjs/axios'],
   build: {
-    extend(config, ctx) {}
-  }
+    babel: {
+      compact: true,
+    },
+  },
 }
